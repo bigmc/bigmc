53c6f92b5f54f2155ab37ec6268c84bb1b0b1b8d
Gian Perrone <gian.perrone@gmail.com>
2011-08-01 09:38:00 +0200

    Added missing header and footer for manual

---
+++ doc/website/manual/footer.php
@@ -0,0 +1,18 @@
+</div>
+	
+<div id="left">
+	<div class="box">
+	<?php include('Table-of-Contents.inc'); ?>
+	</div>
+	<div class="box">
+<p>This work funded in part by the Danish Research Agency (grant no.:  2106-080046)
+       and the <a href="http://itu.dk">IT University of Copenhagen</a> (the <a href="http://www.itu.dk/en/Forskning/Forskningsprojekter/Jingling-Genies">Jingling Genies project</a>)</p>
+		
+		<center>&copy; Copyright 2011 <a href="http://itu.dk/~gdpe/">Gian Perrone</a></center>
+	</div>
+</div>
+</div>
+</body>
+</html>
+
+
---
+++ doc/website/manual/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>User Manual - BigMC Bigraphical Model Checker</title>
+<link rel="stylesheet" type="text/css" href="../style.css" media="screen" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10255629-8']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+<div id="header">
+<h1>BigMC -- Bigraphical Model Checker</h1>
+ <div id="menu">
+  <ul id="nav">
+   <li><a href="/bigmc/">Home</a></li>
+   <li><a href="index.php">Manual</a></li>
+   <li><a href="https://github.com/bigmc/bigmc">GitHub</a></li>
+  </ul>
+ </div>
+</div>
+<div id="content">
+<div id="right">
+
